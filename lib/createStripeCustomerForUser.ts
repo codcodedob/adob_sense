8a00bd5af4627dfaef8aff3486cfeb73fdec2ab6
Donte <burks.donte@gmail.com>
2025-08-15 19:39:50 -0400

    Fix isAlbum trackCount ordering and disable tiles without playable URL

--- lib/createStripeCustomerForUser.ts
+++ lib/createStripeCustomerForUser.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import * as admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: "2024-06-20" });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: "2025-07-30.basil" });
 
 if (!admin.apps.length) {
   admin.initializeApp({
